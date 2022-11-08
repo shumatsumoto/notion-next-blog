043ac14ab9c1600297f0a70842fb7d2148309354
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-08 10:47:02 +0900

    sample データ表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
+import { sampleCards } from "../utils/sample";
 
 export default function Home() {
   return (
@@ -7,7 +8,9 @@ export default function Home() {
       <div className="pt-12">
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
-          {/* Card */}
+          {sampleCards.map((page, index) => (
+            <p key={index}>{page.name}</p>
+          ))}
         </div>
       </div>
     </Layout>
