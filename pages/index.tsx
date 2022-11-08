50d89357df8d6026cea99c14c015c537fb3bb3dc
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-08 11:32:51 +0900

    cardコンポーネント作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Card from "../components/Card";
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
 import { sampleCards } from "../utils/sample";
@@ -9,7 +10,7 @@ export default function Home() {
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
           {sampleCards.map((page, index) => (
-            <p key={index}>{page.name}</p>
+            <Card key={index} page={page} />
           ))}
         </div>
       </div>
