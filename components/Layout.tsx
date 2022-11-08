fbc3613f5e083516ee2b3f26f703deabfc6c6374
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-08 10:18:10 +0900

    top作成（children）

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { FC } from "react";
+import { LayoutProps } from "../types/types";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const Layout = () => {
+const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="relative overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         <Navbar />
-        <main></main>
+        <main className="w-full pb-12 px-4">{children}</main>
         <Footer />
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
 import Layout from "../components/Layout";
+import { siteConfig } from "../site.config";
 
 export default function Home() {
-  return <Layout />;
+  return (
+    <Layout>
+      <div className="pt-12">
+        <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
+        <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
+          {/* Card */}
+        </div>
+      </div>
+    </Layout>
+  );
 }
