5d0dd477c0de9e8d3b32605b84a589d70b1a7e8e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-09 09:40:46 +0900

    indexPage types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { GetStaticProps } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Card from "../components/Card";
 import Layout from "../components/Layout";
 import { siteConfig } from "../site.config";
+import { IndexProps } from "../types/types";
 import { fetchPages } from "../utils/notion";
 import { sampleCards } from "../utils/sample";
 
@@ -15,13 +16,14 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export default function Home({ pages }) {
+const Home: NextPage<IndexProps> = ({ pages }) => {
   console.log(pages);
   return (
     <Layout>
       <div className="pt-12">
         <h1 className="text-5xl mb-8">{siteConfig.title}</h1>
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
+          {/* Card */}
           {sampleCards.map((page, index) => (
             <Card key={index} page={page} />
           ))}
@@ -29,4 +31,6 @@ export default function Home({ pages }) {
       </div>
     </Layout>
   );
-}
+};
+
+export default Home;
