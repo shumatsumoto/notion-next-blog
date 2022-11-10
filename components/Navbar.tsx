7499ddbb069856a0805159480424c180d6cb3211
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-10 12:00:42 +0900

    不要なコード削除

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { siteConfig } from "../site.config";
+import Breadcrumb from "./Breadcrumb";
 
 const Navbar = () => {
   return (
@@ -11,11 +12,12 @@ const Navbar = () => {
           aria-label="breadcrumb"
         >
           <Link href="/">
-            <div className="text-gray-500 hover:text-gray-600">
+            <span className="text-gray-500 hover:text-gray-600">
               {siteConfig.title}
-            </div>
+            </span>
           </Link>
           {/* Breadcrumb */}
+          <Breadcrumb />
         </div>
       </div>
     </nav>
