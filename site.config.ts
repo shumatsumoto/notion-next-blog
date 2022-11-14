997e64b73c804f9620e78d8adced945fd84941c0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-15 00:58:32 +0900

    update sns

--- site.config.ts
+++ site.config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: "My Recipe",
   twitterUrl: "https://twitter.com/shumatsumoto328",
   instagramUrl: "https://www.instagram.com/shumatsumoto0328/",
-  githubUrl: "",
+  githubUrl: "https://github.com/shumatsumoto",
 };
