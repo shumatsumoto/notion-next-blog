9c7daa371baf08cc599791b6e1a193af3893f6cd
Shu Matsumoto <shumatsumoto0328@gmail.com>
2022-11-15 22:46:55 +0900

    meta, favicon設置

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React, { FC } from "react";
 import { LayoutProps } from "../types/types";
 import Footer from "./Footer";
@@ -5,13 +6,22 @@ import Navbar from "./Navbar";
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="relative overflow-hidden">
-      <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
-        <Navbar />
-        <main className="w-full pb-12 px-4">{children}</main>
-        <Footer />
+    <>
+      <Head>
+        <title>My Recipe</title>
+        <meta title="My Recipe" />
+        <meta name="keywords" content="My Recipe, 和食, 洋食, パン, ケーキ, ちいかわ"></meta>
+        <meta name="description" content="My Recipeです。ちいかわちゃんと仲良くクッキング。楽しい！！"></meta>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></link>
+      </Head>
+      <div className="relative overflow-hidden">
+        <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
+          <Navbar />
+          <main className="w-full pb-12 px-4">{children}</main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
